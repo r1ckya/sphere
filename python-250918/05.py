ea5a62add76342b50740ab5067dd6de285eb9e1f
r1ckya <geraskin.ia@yandex.ru>
2018-09-25 23:47:23 +0300

    hw 250918

--- python-250918/05.py
+++ python-250918/05.py
@@ -5,11 +5,11 @@ import pandas as pd
 def df_diag_ones(df):
     df.values[range(df.shape[0]), range(df.shape[1])] = 0
     df.values[range(df.shape[0]), range(df.shape[1] - 1, -1, -1)] = 0
-    return df
+    return df.values
 
 if __name__ == '__main__':
     df = []
     for line in sys.stdin.readlines():
         df.append(list(map(int, line.split())))
     df = pd.DataFrame(df)
-    print(df_diag_ones(df).values)
+    print(df_diag_ones(df))
