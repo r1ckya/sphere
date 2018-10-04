d372e52693e949da300760b70ee9e1096e48cf73
rickya <geraskin.ia@yandex.ru>
2018-10-04 23:10:31 +0300

    binominal -> binomial

--- python-021018/06.py
+++ python-021018/06.py
@@ -1,14 +1,9 @@
-def binominal(n):
+def binomial(n):
     cur = 1
     for k in range(n + 1):
         yield cur
         cur = (n - k) * cur // (k + 1)
 
 if __name__ == '__main__':
-    from sys import stdin
-    for line in stdin.readlines():
-        for n in map(int, line.split()):
-            for x in binominal(n):
-                print(x, end=' ', sep=' ')
-            print()
-
+    for n in map(int, input().split()):
+        print(*binomial(n))
